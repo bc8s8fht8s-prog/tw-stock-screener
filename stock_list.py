2869a5752843f1d3089ad119de3f5c4e4292c405
Chen-yu Lee <yokohama307@gmail.com>
2026-07-06 16:31:46 +0800

    Use local stock list

--- stock_list.py
+++ stock_list.py
@@ -1,42 +1,8 @@
 import pandas as pd
-import requests
-
-TWSE_URL = "https://openapi.twse.com.tw/v1/opendata/t187ap03_L"
 
 
 def get_stock_list():
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
+    df = pd.read_csv("stock_list.csv", dtype=str)
 
-    r = requests.get(
-        TWSE_URL,
-        headers=headers,
-        timeout=30
-    )
-
-    r.raise_for_status()
-
-    if not r.text.strip():
-        raise Exception("TWSE API 回傳空白內容")
-
-    r.raise_for_status()
-
-    print("HTTP Status:", r.status_code)
-    print("Content-Type:", r.headers.get("Content-Type"))
-    print("Response:")
-    print(r.text[:500])
-
-    df = pd.DataFrame(r.json())
-
-    df = df.rename(columns={
-        "公司代號": "code",
-        "公司簡稱": "name",
-    })
-
-    return (
-        df[["code", "name"]]
-        .sort_values("code")
-        .reset_index(drop=True)
-    )
+    return df.sort_values("code").reset_index(drop=True)
